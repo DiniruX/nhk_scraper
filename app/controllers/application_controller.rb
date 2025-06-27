33b6272e2cb4cd8202b1f7bc2a19b845008c209c
Diniru Sandipa <it20041120@my.sliit.lk>
2025-06-28 02:53:52 +0900

    deployment configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
+# class ApplicationController < ActionController::Base
+#   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+#   allow_browser versions: :modern
+# end
+
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << "nhk-scraper-db.c5wcg0igoaim.ap-southeast-2.rds.amazonaws.com"
+  config.require_master_key = true
 end
